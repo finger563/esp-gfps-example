191b4337ca156e61575597f1a253edb650871ea9
William Emfinger <waemfinger@gmail.com>
2023-08-11 11:37:02 -0500

    feat(gfps): swapped out example for gfps * Swapped out example services / characteristics for GFPS service and characteristics. Shows up properly in ble scanner, but does not trigger any read by nearby android phones so likely something is missing / incorrect w.r.t. the adv. data.

--- main/main.cpp
+++ main/main.cpp
@@ -20,23 +20,30 @@ static espp::Logger logger({.tag = "GFPS Example", .level = espp::Logger::Verbos
 enum
   {
     IDX_SVC,
-    IDX_CHAR_A,
-    IDX_CHAR_VAL_A,
-    IDX_CHAR_CFG_A,
+    IDX_CHAR_MODEL_ID,
+    IDX_CHAR_VAL_MODEL_ID,
 
-    IDX_CHAR_B,
-    IDX_CHAR_VAL_B,
+    IDX_CHAR_KB_PAIRING,
+    IDX_CHAR_VAL_KB_PAIRING,
+    IDX_CHAR_CFG_KB_PAIRING,
 
-    IDX_CHAR_C,
-    IDX_CHAR_VAL_C,
+    IDX_CHAR_PASSKEY,
+    IDX_CHAR_VAL_PASSKEY,
+    IDX_CHAR_CFG_PASSKEY,
 
-    HRS_IDX_NB,
+    IDX_CHAR_ACCOUNT_KEY,
+    IDX_CHAR_VAL_ACCOUNT_KEY,
+
+    IDX_CHAR_FW_REVISION,
+    IDX_CHAR_VAL_FW_REVISION,
+
+    GFPS_IDX_NB,
   };
 
 #define PROFILE_NUM                 1
 #define PROFILE_APP_IDX             0
 #define ESP_APP_ID                  0x55
-#define SAMPLE_DEVICE_NAME          "ESP_GATTS_DEMO"
+#define SAMPLE_DEVICE_NAME          "GFPS DEMO"
 #define SVC_INST_ID                 0
 
 /* The max length of characteristic value. When the GATT client performs a write or prepare write operation,
@@ -51,7 +58,7 @@ enum
 
 static uint8_t adv_config_done       = 0;
 
-uint16_t heart_rate_handle_table[HRS_IDX_NB];
+uint16_t gfps_handle_table[GFPS_IDX_NB];
 
 typedef struct {
   uint8_t                 *prepare_buf;
@@ -70,7 +77,7 @@ static uint8_t raw_adv_data[] = {
   /* service uuid */
   0x03, 0x03, 0xFF, 0x00,
   /* device name */
-  0x0f, 0x09, 'E', 'S', 'P', '_', 'G', 'A', 'T', 'T', 'S', '_', 'D','E', 'M', 'O'
+  0x0A, 0x09, 'G', 'F', 'P', 'S', ' ', 'D','E', 'M', 'O'
 };
 static uint8_t raw_scan_rsp_data[] = {
   /* flags */
@@ -84,7 +91,8 @@ static uint8_t raw_scan_rsp_data[] = {
 static uint8_t service_uuid[16] = {
   /* LSB <--------------------------------------------------------------------------------> MSB */
   //first uuid, 16bit, [12],[13] is the value
-  0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+  // 0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
+  0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x2C, 0xFE, 0x00, 0x00,
 };
 
 /* The length of adv data must be less than 31 bytes */
@@ -150,7 +158,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event,
                                         esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
 
 /* One gatt-based profile one app_id and one gatts_if, this array will store the gatts_if returned by ESP_GATTS_REG_EVT */
-static struct gatts_profile_inst heart_rate_profile_tab[PROFILE_NUM] = {
+static struct gatts_profile_inst gfps_profile_tab[PROFILE_NUM] = {
   [PROFILE_APP_IDX] = {
     .gatts_cb = gatts_profile_event_handler,
     .gatts_if = ESP_GATT_IF_NONE,       /* Not get the gatt_if, so initial is ESP_GATT_IF_NONE */
@@ -158,63 +166,140 @@ static struct gatts_profile_inst heart_rate_profile_tab[PROFILE_NUM] = {
 };
 
 /* Service */
-static const uint16_t GATTS_SERVICE_UUID_TEST      = 0x00FF;
-static const uint16_t GATTS_CHAR_UUID_TEST_A       = 0xFF01;
-static const uint16_t GATTS_CHAR_UUID_TEST_B       = 0xFF02;
-static const uint16_t GATTS_CHAR_UUID_TEST_C       = 0xFF03;
+// NOTE: these UUIDs are specified at 16-bit, which means that 1) they are not
+// in reverse order, and 2) the rest of their full 128-bit UUIDs are the standard
+// Bluetooth SIG base UUID (0x0000XXXX-0000-1000-8000-00805F9B34FB), and the 16-bit
+// UUIDs are the last 2 bytes of the first 4 bytes of the full UUIDs (XXXX).
+static const uint16_t GATTS_SERVICE_UUID_GFPS            = 0xFE2C;
+static const uint16_t GATTS_CHAR_UUID_GFPS_FW_REVISION   = 0x2A26;
+
+// NOTE: the UUIDs are in reverse order
+static const uint8_t GATTS_CHAR_UUID_GFPS_MODEL_ID[16]      = {0xEA, 0x0B, 0x10, 0x32,
+                                                               0xDE, 0x01, 0xB0, 0x8E,
+                                                               0x14, 0x48, 0x66, 0x83,
+                                                               0x33, 0x12, 0x2C, 0xFE};
+static const uint8_t GATTS_CHAR_UUID_GFPS_KB_PAIRING[16]    = {0xEA, 0x0B, 0x10, 0x32,
+                                                               0xDE, 0x01, 0xB0, 0x8E,
+                                                               0x14, 0x48, 0x66, 0x83,
+                                                               0x34, 0x12, 0x2C, 0xFE};
+static const uint8_t GATTS_CHAR_UUID_GFPS_PASSKEY[16]       = {0xEA, 0x0B, 0x10, 0x32,
+                                                               0xDE, 0x01, 0xB0, 0x8E,
+                                                               0x14, 0x48, 0x66, 0x83,
+                                                               0x35, 0x12, 0x2C, 0xFE};
+static const uint8_t GATTS_CHAR_UUID_GFPS_ACCOUNT_KEY[16]   = {0xEA, 0x0B, 0x10, 0x32,
+                                                               0xDE, 0x01, 0xB0, 0x8E,
+                                                               0x14, 0x48, 0x66, 0x83,
+                                                               0x36, 0x12, 0x2C, 0xFE};
 
 static const uint16_t primary_service_uuid         = ESP_GATT_UUID_PRI_SERVICE;
 static const uint16_t character_declaration_uuid   = ESP_GATT_UUID_CHAR_DECLARE;
 static const uint16_t character_client_config_uuid = ESP_GATT_UUID_CHAR_CLIENT_CONFIG;
-static const uint8_t char_prop_read                =  ESP_GATT_CHAR_PROP_BIT_READ;
+static const uint8_t char_prop_read                = ESP_GATT_CHAR_PROP_BIT_READ;
 static const uint8_t char_prop_write               = ESP_GATT_CHAR_PROP_BIT_WRITE;
 static const uint8_t char_prop_read_write_notify   = ESP_GATT_CHAR_PROP_BIT_WRITE | ESP_GATT_CHAR_PROP_BIT_READ | ESP_GATT_CHAR_PROP_BIT_NOTIFY;
-static const uint8_t heart_measurement_ccc[2]      = {0x00, 0x00};
-static const uint8_t char_value[4]                 = {0x11, 0x22, 0x33, 0x44};
 
+// TODO: update
+static const uint8_t ccc[2]           = {0x00, 0x03}; // LSb corresponds to notifications (1 if enabled, 0 if disabled), next bit (bit 1) corresponds to indications - 1 if enabled, 0 if disabled
+static const uint8_t model_id[3]      = {0x62, 0xEF, 0x1C}; // 0x62EF1C
+static const uint8_t fw_revision[4]   = {'1', '.', '0', 0x00};
+static const uint8_t char_value[16]    = {0x00};
+
+struct KbPairing {
+  uint8_t encrypted_request[16]; //!< 16 byte encrypted request
+  uint8_t public_key[64];        //!< 64 byte public key (optional)
+} __attribute__((packed));
+
+struct EncryptedRequest {
+  uint8_t message_type; //!< One of (0x00 = key based pairing reqeust)
+  uint8_t flags;        //!< Bit 0 = deprecated,
+                        //   Bit 1 = true if seeker reqeusts provider initiate bonding, and this request contains seekers BR/EDR address
+                        //   Bit 2 = true if seeker requests provider notify the existing name
+                        //   Bit 3 = true if this is for retroactively writing account key
+                        //   Bits 4-7 = reserved
+  uint8_t provider_address[6]; //!< Either providers current BLE address or providers public address
+  uint8_t seeker_address[6];   //!< Seekers BR/EDR address
+  uint8_t remaining_salt_bytes[2]; //!< Remaining bytes of salt
+} __attribute__((packed));
+
+// this structure is encrypted and used with the CHAR_UUID_GFPS_PASSKEY
+struct Passkey {
+  uint8_t message_type; //!< One of (0x02 = seeker's passkey, 0x03 = provider's passkey)
+  uint32_t passkey;     //!< 6 digit passkey
+  uint8_t random_value[12]; //!< 12 byte random value (salt)
+} __attribute__((packed));
 
 /* Full Database Description - Used to add attributes into the database */
-static const esp_gatts_attr_db_t gatt_db[HRS_IDX_NB] =
+static const esp_gatts_attr_db_t gatt_db[GFPS_IDX_NB] =
   {
     // Service Declaration
     [IDX_SVC]        =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&primary_service_uuid, ESP_GATT_PERM_READ,
-                           sizeof(uint16_t), sizeof(GATTS_SERVICE_UUID_TEST), (uint8_t *)&GATTS_SERVICE_UUID_TEST}},
+                           sizeof(uint16_t), sizeof(GATTS_SERVICE_UUID_GFPS), (uint8_t *)&GATTS_SERVICE_UUID_GFPS}},
 
     /* Characteristic Declaration */
-    [IDX_CHAR_A]     =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
-                           CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read_write_notify}},
-
-    /* Characteristic Value */
-    [IDX_CHAR_VAL_A] =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_A, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
-
-    /* Client Characteristic Configuration Descriptor */
-    [IDX_CHAR_CFG_A]  =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_client_config_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-                           sizeof(uint16_t), sizeof(heart_measurement_ccc), (uint8_t *)heart_measurement_ccc}},
-
-    /* Characteristic Declaration */
-    [IDX_CHAR_B]      =
+    [IDX_CHAR_MODEL_ID]     =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
                            CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read}},
 
     /* Characteristic Value */
-    [IDX_CHAR_VAL_B]  =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_B, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
-                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
+    [IDX_CHAR_VAL_MODEL_ID] =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_128, (uint8_t *)GATTS_CHAR_UUID_GFPS_MODEL_ID, ESP_GATT_PERM_READ,
+                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(model_id), (uint8_t *)model_id}},
 
     /* Characteristic Declaration */
-    [IDX_CHAR_C]      =
+    [IDX_CHAR_KB_PAIRING]      =
     {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
+                           CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read_write_notify}},
+
+    // TODO: UPDATE
+    /* Characteristic Value */
+    [IDX_CHAR_VAL_KB_PAIRING]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_128, (uint8_t *)GATTS_CHAR_UUID_GFPS_KB_PAIRING, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
+
+    // TODO: UPDATE
+    /* Client Characteristic Configuration Descriptor */
+    [IDX_CHAR_CFG_KB_PAIRING]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_client_config_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+                           sizeof(uint16_t), sizeof(ccc), (uint8_t *)ccc}},
+
+    /* Characteristic Declaration */
+    [IDX_CHAR_PASSKEY]      =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
+                           CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read_write_notify}},
+
+    // TODO: UPDATE
+    /* Characteristic Value */
+    [IDX_CHAR_VAL_PASSKEY]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_128, (uint8_t *)GATTS_CHAR_UUID_GFPS_PASSKEY, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
+
+    // TODO: UPDATE
+    /* Client Characteristic Configuration Descriptor */
+    [IDX_CHAR_CFG_PASSKEY]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_client_config_uuid, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+                           sizeof(uint16_t), sizeof(ccc), (uint8_t *)ccc}},
+
+    /* Characteristic Declaration */
+    [IDX_CHAR_ACCOUNT_KEY]      =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_WRITE,
                            CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_write}},
 
     /* Characteristic Value */
-    [IDX_CHAR_VAL_C]  =
-    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_TEST_C, ESP_GATT_PERM_READ | ESP_GATT_PERM_WRITE,
+    [IDX_CHAR_VAL_ACCOUNT_KEY]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_128, (uint8_t *)GATTS_CHAR_UUID_GFPS_ACCOUNT_KEY, ESP_GATT_PERM_WRITE,
                            GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(char_value), (uint8_t *)char_value}},
+
+    /* Characteristic Declaration */
+    [IDX_CHAR_FW_REVISION]      =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&character_declaration_uuid, ESP_GATT_PERM_READ,
+                           CHAR_DECLARATION_SIZE, CHAR_DECLARATION_SIZE, (uint8_t *)&char_prop_read}},
+
+    /* Characteristic Value */
+    [IDX_CHAR_VAL_FW_REVISION]  =
+    {{ESP_GATT_AUTO_RSP}, {ESP_UUID_LEN_16, (uint8_t *)&GATTS_CHAR_UUID_GFPS_FW_REVISION, ESP_GATT_PERM_READ,
+                           GATTS_DEMO_CHAR_VAL_LEN_MAX, sizeof(fw_revision), (uint8_t *)fw_revision}},
+
 
   };
 
@@ -370,7 +455,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
     }
     adv_config_done |= SCAN_RSP_CONFIG_FLAG;
 #endif
-    esp_err_t create_attr_ret = esp_ble_gatts_create_attr_tab(gatt_db, gatts_if, HRS_IDX_NB, SVC_INST_ID);
+    esp_err_t create_attr_ret = esp_ble_gatts_create_attr_tab(gatt_db, gatts_if, GFPS_IDX_NB, SVC_INST_ID);
     if (create_attr_ret){
       logger.error("create attr table failed, error code = {:x}", create_attr_ret);
     }
@@ -383,8 +468,9 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
     if (!param->write.is_prep){
       // the data length of gattc write  must be less than GATTS_DEMO_CHAR_VAL_LEN_MAX.
       logger.info("GATT_WRITE_EVT, handle = {}, value len = {}, value :", param->write.handle, param->write.len);
+      // TODO: UPDATE
       // esp_log_buffer_hex("", param->write.value, param->write.len);
-      if (heart_rate_handle_table[IDX_CHAR_CFG_A] == param->write.handle && param->write.len == 2){
+      if (gfps_handle_table[IDX_CHAR_CFG_KB_PAIRING] == param->write.handle && param->write.len == 2){
         uint16_t descr_value = param->write.value[1]<<8 | param->write.value[0];
         if (descr_value == 0x0001){
           logger.info("notify enable");
@@ -394,7 +480,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
               notify_data[i] = i % 0xff;
             }
           //the size of notify_data[] need less than MTU size
-          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, heart_rate_handle_table[IDX_CHAR_VAL_A],
+          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gfps_handle_table[IDX_CHAR_VAL_KB_PAIRING],
                                       sizeof(notify_data), notify_data, false);
         }else if (descr_value == 0x0002){
           logger.info("indicate enable");
@@ -404,7 +490,7 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
               indicate_data[i] = i % 0xff;
             }
           //the size of indicate_data[] need less than MTU size
-          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, heart_rate_handle_table[IDX_CHAR_VAL_A],
+          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gfps_handle_table[IDX_CHAR_VAL_KB_PAIRING],
                                       sizeof(indicate_data), indicate_data, true);
         }
         else if (descr_value == 0x0000){
@@ -413,7 +499,37 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
           logger.error("unknown descr value");
           // esp_log_buffer_hex("", param->write.value, param->write.len);
         }
-
+      }
+      // TODO: UPDATE
+      else if (gfps_handle_table[IDX_CHAR_CFG_PASSKEY] == param->write.handle && param->write.len == 2){
+        uint16_t descr_value = param->write.value[1]<<8 | param->write.value[0];
+        if (descr_value == 0x0001){
+          logger.info("notify enable");
+          uint8_t notify_data[15];
+          for (int i = 0; i < sizeof(notify_data); ++i)
+            {
+              notify_data[i] = i % 0xff;
+            }
+          //the size of notify_data[] need less than MTU size
+          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gfps_handle_table[IDX_CHAR_VAL_PASSKEY],
+                                      sizeof(notify_data), notify_data, false);
+        }else if (descr_value == 0x0002){
+          logger.info("indicate enable");
+          uint8_t indicate_data[15];
+          for (int i = 0; i < sizeof(indicate_data); ++i)
+            {
+              indicate_data[i] = i % 0xff;
+            }
+          //the size of indicate_data[] need less than MTU size
+          esp_ble_gatts_send_indicate(gatts_if, param->write.conn_id, gfps_handle_table[IDX_CHAR_VAL_PASSKEY],
+                                      sizeof(indicate_data), indicate_data, true);
+        }
+        else if (descr_value == 0x0000){
+          logger.info("notify/indicate disable ");
+        }else{
+          logger.error("unknown descr value");
+          // esp_log_buffer_hex("", param->write.value, param->write.len);
+        }
       }
       /* send response when param->write.need_rsp is true*/
       if (param->write.need_rsp){
@@ -460,14 +576,14 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
     if (param->add_attr_tab.status != ESP_GATT_OK){
       logger.error("create attribute table failed, error code=0x{:x}", (int)param->add_attr_tab.status);
     }
-    else if (param->add_attr_tab.num_handle != HRS_IDX_NB){
+    else if (param->add_attr_tab.num_handle != GFPS_IDX_NB){
       logger.error("create attribute table abnormally, num_handle ({}) \
-                        doesn't equal to HRS_IDX_NB({})", (int)param->add_attr_tab.num_handle, (int)HRS_IDX_NB);
+                        doesn't equal to GFPS_IDX_NB({})", (int)param->add_attr_tab.num_handle, (int)GFPS_IDX_NB);
     }
     else {
       logger.info("create attribute table successfully, the number handle = {}", (int)param->add_attr_tab.num_handle);
-      memcpy(heart_rate_handle_table, param->add_attr_tab.handles, sizeof(heart_rate_handle_table));
-      esp_ble_gatts_start_service(heart_rate_handle_table[IDX_SVC]);
+      memcpy(gfps_handle_table, param->add_attr_tab.handles, sizeof(gfps_handle_table));
+      esp_ble_gatts_start_service(gfps_handle_table[IDX_SVC]);
     }
     break;
   }
@@ -490,7 +606,7 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
   /* If event is register event, store the gatts_if for each profile */
   if (event == ESP_GATTS_REG_EVT) {
     if (param->reg.status == ESP_GATT_OK) {
-      heart_rate_profile_tab[PROFILE_APP_IDX].gatts_if = gatts_if;
+      gfps_profile_tab[PROFILE_APP_IDX].gatts_if = gatts_if;
     } else {
       logger.error("reg app failed, app_id {:04x}, status {}",
                    (int)param->reg.app_id,
@@ -502,9 +618,9 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
     int idx;
     for (idx = 0; idx < PROFILE_NUM; idx++) {
       /* ESP_GATT_IF_NONE, not specify a certain gatt_if, need to call every profile cb function */
-      if (gatts_if == ESP_GATT_IF_NONE || gatts_if == heart_rate_profile_tab[idx].gatts_if) {
-        if (heart_rate_profile_tab[idx].gatts_cb) {
-          heart_rate_profile_tab[idx].gatts_cb(event, gatts_if, param);
+      if (gatts_if == ESP_GATT_IF_NONE || gatts_if == gfps_profile_tab[idx].gatts_if) {
+        if (gfps_profile_tab[idx].gatts_cb) {
+          gfps_profile_tab[idx].gatts_cb(event, gatts_if, param);
         }
       }
     }
